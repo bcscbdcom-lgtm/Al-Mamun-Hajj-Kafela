6e464f85534c6a17b72a61b3505d0d7ec69ae616
bcscbdcom-lgtm <bcscbd.com@gmail.com>
2026-09-05 13:52:31 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Al-Mamun-Hajj-Kafela/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
